b0b10836a8f7e4115425aebc2272afd8be7c79ca
Sandy Steele <Sandy_steele@hotmail.com>
2020-11-18 13:53:20 +0000

    Character Switching added
    Should be done

---
+++ Assets/Scripts/Characters/FollowCam.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCam : MonoBehaviour
+{
+    public Character following;
+    public float speed = 0.1f;
+
+    private void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float newX = Mathf.Lerp(transform.position.x, following.transform.position.x, speed);
+        float newY = Mathf.Lerp(transform.position.y, following.transform.position.y, speed);
+        float zValue = transform.position.z;
+
+        transform.position = new Vector3(newX, newY, zValue);
+    }
+
+    public void updateFollowing(Character following)
+    {
+        this.following = following;
+    }
+}
--- Assets/Scripts/Characters/Switch.cs
+++ Assets/Scripts/Characters/Switch.cs
@@ -4,15 +4,56 @@ using UnityEngine;
 
 public class Switch : MonoBehaviour
 {
+    public Character[] characters;
+    public FollowCam cam;
+    private int index = 0;
+
+    public int coolDown = 30;
+    private int timer = 0;
+
+    private bool canSwitch = true;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        //Switch left and right
+        if(canSwitch && Input.GetAxisRaw("SwitchLeft") > 0)
+        {
+            index--;
+            canSwitch = false;
+        } else if(canSwitch && Input.GetAxisRaw("SwitchRight") > 0)
+        {
+            index++;
+            canSwitch = false;
+        }
+
+
+        if (!canSwitch)
+        {
+            timer++;
+        }
+
+        if(timer >= coolDown)
+        {
+            canSwitch = true;
+            timer = 0;
+        }
+
+        //Loop round the list
+        if(index < 0)
+        {
+            index = characters.Length - 1;
+        } else if(index >= characters.Length)
+        {
+            index = 0;
+        }
+
+        cam.updateFollowing(characters[index]);
     }
 }
