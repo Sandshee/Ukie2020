fc0120d1f1e3121c3f17a048bc7194a20421cced
Sandy Steele <Sandy_steele@hotmail.com>
2020-11-18 14:01:20 +0000

    Moved BasicMove to character
    Moved code from BasicMove to Character

--- Assets/Scripts/Characters/Character.cs
+++ Assets/Scripts/Characters/Character.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 
 public class Character : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float movementSpeed;
+    private Rigidbody2D rb2d;
+
     void Start()
     {
-        
+        rb2d = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+        rb2d.AddForce(movement * movementSpeed);
     }
 }
