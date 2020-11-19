e898260265d07a65eeaac1845967225469c6cbd6
Sandy Steele <Sandy_steele@hotmail.com>
2020-11-19 12:38:19 +0000

    LevelAvailable
    Team members can now customise and create their own levels

---
+++ Assets/Scripts/Levels/Door.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    private Animator anim;
+    private BoxCollider2D coll;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        coll = GetComponent<BoxCollider2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void open()
+    {
+        anim.SetBool("Up", true);
+        coll.enabled = false;
+    }
+
+    public void close()
+    {
+        anim.SetBool("Up", false);
+        coll.enabled = true;
+    }
+}
